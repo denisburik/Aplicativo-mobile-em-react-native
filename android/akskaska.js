2bec3130ee4f6f6f0d98b9aed03fd712e8b6685a
Denis Leonardo <denis@epics.com.br>
2020-08-20 12:35:37 -0300

    Iniciando o aplicativo

---
+++ android/akskaska.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    var data = [Response]
+
+    $.ajax({
+        type: 'POST',
+        url: 'No caso seria a pagina para exibir os dados',
+        data: data
+    })
+
+})
